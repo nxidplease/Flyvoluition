821c6067d28e28084c9b6a7a6315223cf2ba60c1
Anatoli Krasilshikov <ooTehDudeoo@gmail.com>
2016-07-31 20:24:41 +0300

    Added Fly model + texture, reformatted the MainGameLoop. In the future please press Ctrl + I to format the file.

--- GameEngine/src/engineTester/MainGameLoop.java
+++ GameEngine/src/engineTester/MainGameLoop.java
@@ -27,11 +27,11 @@ import entities.Farmer;
 public class MainGameLoop {
 
 	public static void main(String[] args) {
-		
+
 		DisplayManger.createDisplay();
-		
+
 		Loader loader = new Loader();
-		
+
 		//RawModel model = HisObjLoader.loadOBJ("MyGayzerModel", loader);
 		RawModel model = MyObjLoader.loadObjModel("dragon", loader);
 		//model = HisOldObjLoader.loadObjModel("MyGayzerModel", loader);
@@ -39,7 +39,7 @@ public class MainGameLoop {
 		TexturedModel staticModel = new TexturedModel(model, texture);
 		texture.setShineDamper(10);
 		texture.setReflectivity(1);
-		
+
 		ModelTexture grass = new ModelTexture(loader.loadTexture("grassTexture"));
 		grass.setHasTransparency(true);
 		grass.setUseFakeLighting(true);
@@ -49,75 +49,75 @@ public class MainGameLoop {
 		RawModel fernModel = MyObjLoader.loadObjModel("fern", loader);
 		TexturedModel grassTexturedModel = new TexturedModel(grassModel, grass);
 		TexturedModel fernTexturedModel = new TexturedModel(fernModel, fern);
-		
+
 		Entity entity = new Entity(staticModel, new Vector3f(0, 0, -5), 0, 0, 0, 1);
 		Light light = new Light(new Vector3f(1, 1, 1), new Vector3f(400, 600, 400));
-		
+
 		// Terrain Texture Pack Creation
 		TerrainTexture backTexture = new TerrainTexture(loader.loadTexture("Dirt"));
 		TerrainTexture rTexture = new TerrainTexture(loader.loadTexture("grassBack"));
 		TerrainTexture gTexture = new TerrainTexture(loader.loadTexture("Snow"));
 		TerrainTexture bTexture = new TerrainTexture(loader.loadTexture("Ice"));
-		
+
 		TerrainTexturePack textPack = new TerrainTexturePack(backTexture, rTexture, gTexture, bTexture);
-		
+
 		TerrainTexture blendMap = new TerrainTexture(loader.loadTexture("blendMap"));
-		
-		
-		
+
+
+
 		Terrain terrain = new Terrain(0, 0, loader, textPack, blendMap);
 		Terrain terrain1 = new Terrain(1, 0, loader, textPack, blendMap);
 		RawModel bunny = MyObjLoader.loadObjModel("stanfordBunny", loader);
 		TexturedModel bunnyModel = new TexturedModel(bunny, new ModelTexture(loader.loadTexture("Snow")));
-		
+
 		Player player = new Player(bunnyModel, new Vector3f(0, 0, 0),0 ,0 , 0, 1);
 		Camera camera = new Camera(player);
-		
-		
-		
+
+
+
 		List<Entity> entities = new ArrayList<>();
 		Random rnd = new Random();
-		
+
 		for(int i = 0; i < 200; i++){
-			
+
 			entities.add(new Entity(grassTexturedModel, new Vector3f(rnd.nextFloat() * 400,0, rnd.nextFloat() * 400), 0, 0, 0, 3));
 			entities.add(new Entity(fernTexturedModel, new Vector3f(rnd.nextFloat() * 400,0,rnd.nextFloat() * 400), 0, 0, 0, 1));
 		}
-		
+
 		MasterRenderer renderer = new MasterRenderer();
-		
-		
-		
-//*********FARMERS**************
-			RawModel personModel = MyObjLoader.loadObjModel("person", loader);
-			TexturedModel texturedPersonModel = new TexturedModel(personModel, new ModelTexture(loader.loadTexture("image")));
 
-		
-			List<Farmer> farmers = new ArrayList<>();
-			for(int i = 0; i < 200; i++)
-				farmers.add(new Farmer(texturedPersonModel, new Vector3f(), 0, 0, 0, 1));
-				
-//*********END FARMERS************************
-			
-			
-//**********TREES****************************			
-RawModel treeModel = MyObjLoader.loadObjModel("treeModel", loader);
-ModelTexture tree = new ModelTexture(loader.loadTexture("tree"));
-TexturedModel treeTexturedModel = new TexturedModel(treeModel, tree);
 
-List<Entity> trees = new ArrayList<>();
 
-for(int i = 0; i < 200; i++){
-	trees.add(  new Entity(treeTexturedModel, new Vector3f(rnd.nextFloat() * 800,0,rnd.nextFloat() * 800), 0, 0, 0, (rnd.nextInt(4)+10))    );
-}	
-			
-	
-			
-//**********END TREES****************************					
-		
-			
-			
-			
+		//*********FARMERS**************
+		RawModel personModel = MyObjLoader.loadObjModel("Fly", loader);
+		TexturedModel texturedPersonModel = new TexturedModel(personModel, new ModelTexture(loader.loadTexture("Fly")));
+
+
+		List<Farmer> farmers = new ArrayList<>();
+		for(int i = 0; i < 200; i++)
+			farmers.add(new Farmer(texturedPersonModel, new Vector3f(), 0, 0, 0, 5));
+
+		//*********END FARMERS************************
+
+
+		//**********TREES****************************			
+		RawModel treeModel = MyObjLoader.loadObjModel("treeModel", loader);
+		ModelTexture tree = new ModelTexture(loader.loadTexture("tree"));
+		TexturedModel treeTexturedModel = new TexturedModel(treeModel, tree);
+
+		List<Entity> trees = new ArrayList<>();
+
+		for(int i = 0; i < 200; i++){
+			trees.add(  new Entity(treeTexturedModel, new Vector3f(rnd.nextFloat() * 800,0,rnd.nextFloat() * 800), 0, 0, 0, (rnd.nextInt(4)+10))    );
+		}	
+
+
+
+		//**********END TREES****************************					
+
+
+
+
 		while(!Display.isCloseRequested()) {
 			//entity.increasePosition(0, 0, -0.1f);
 			entity.increaseRotation(0.5f, 0.5f, 0);
@@ -127,34 +127,34 @@ for(int i = 0; i < 200; i++){
 			renderer.proccessEntity(player);
 			renderer.proccessTerrain(terrain);
 			renderer.proccessTerrain(terrain1);
-			
+
 
 			for (Entity entity2 : entities) {
 				renderer.proccessEntity(entity2);
 			}
 			renderer.render(light, camera);
-			
+
 			for (Farmer farmer : farmers) {
 				renderer.proccessEntity(farmer);
 				farmer.move();
 			}
-			
+
 			for (Entity aTree : trees) {
 				renderer.proccessEntity(aTree);
 			}
-			
-			
+
+
 			// game logic
 			DisplayManger.updateDisplay();
-		
-			
+
+
 		}
-		
+
 		renderer.cleanUp();
 		loader.cleanUp();
 		DisplayManger.closeDisplay();
 
-	
+
 	}
 
 }
